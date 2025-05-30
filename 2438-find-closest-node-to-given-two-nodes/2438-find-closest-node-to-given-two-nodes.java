e15a9f26b17fff36396e2711b55722f069c71aac
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-05-30 17:58:40 +0530

    Time: 10 ms (98.94%), Space: 57.9 MB (83.39%) - LeetHub

---
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,33 @@
+
+public class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int[] dist1 = getDistances(edges, node1);
+        int[] dist2 = getDistances(edges, node2);
+
+        int result = -1;
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < edges.length; i++) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
+                int maxDist = Math.max(dist1[i], dist2[i]);
+                if (maxDist < minDistance) {
+                    minDistance = maxDist;
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int[] getDistances(int[] edges, int start) {
+        int n = edges.length;
+        int[] dist = new int[n];
+        java.util.Arrays.fill(dist, -1);
+        int d = 0;
+        while (start != -1 && dist[start] == -1) {
+            dist[start] = d++;
+            start = edges[start];
+        }
+        return dist;
+    }
+}
