207a99c063c657c627273174daaa93b68c0fb9fc
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-11-21 20:47:42 +0530

    Time: 31 ms (98.48%), Space: 46.8 MB (30.09%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,27 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int[] R = new int[26];
-        for (char u : s.toCharArray()) {
-            R[u - 'a']++;
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+        for (int i = 0; i < 26; i++) {
+            first[i] = -1;
+            last[i] = -1;
         }
-        int[] L = new int[26];
-        HashSet<Integer> S = new HashSet<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            int t = s.charAt(i) - 'a';
-            R[t]--;
-            for (int j = 0; j < 26; j++) {
-                if (L[j] > 0 && R[j] > 0) {
-                    S.add(26 * t + j);
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            if (first[c] == -1) first[c] = i;
+            last[c] = i;
+        }
+        int ans = 0;
+        for (int c = 0; c < 26; c++) {
+            if (first[c] != -1 && last[c] - first[c] > 1) {
+                int mask = 0;
+                for (int i = first[c] + 1; i < last[c]; i++) {
+                    mask |= 1 << (s.charAt(i) - 'a');
                 }
+                ans += Integer.bitCount(mask);
             }
-            L[t]++;
         }
-
-        return S.size();
+        return ans;
     }
 }
