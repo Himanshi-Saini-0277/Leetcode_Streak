cbc4e1794c81f46571d5497deeb7f9c6e604f8e7
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-12-08 21:10:55 +0530

    Time: 0 ms (100%), Space: 41.6 MB (67.85%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public int countTriples(int n) {
+        int cnt = 0;
+        
+        for (int m = 2; m * m < n; m++) {
+            for (int k = 1; k < m; k++) {
+                if (gcd(m, k) == 1 && (m - k) % 2 == 1) {
+                    int c_primitive = m * m + k * k;
+                    
+                    if (c_primitive > n) {
+                        break;
+                    }
+                    cnt += 2 * (n / c_primitive);
+                }
+            }
+        }
+        return cnt;
+    }
+}
