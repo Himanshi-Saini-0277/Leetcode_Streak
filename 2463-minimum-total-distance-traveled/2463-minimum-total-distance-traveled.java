0faa27ceea27f4e7946df9b9c09ea2b95d270d9d
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2026-04-14 08:58:53 +0530

    Time: 40 ms (66.13%), Space: 44.6 MB (75.81%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    long[][] dp;
+
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        Collections.sort(robot);
+        Arrays.sort(factory, (a, b) -> a[0] - b[0]);
+
+        int n = robot.size();
+        int m = factory.length;
+
+        dp = new long[n][m];
+        for (long[] row : dp) Arrays.fill(row, -1);
+
+        return solve(0, 0, robot, factory);
+    }
+
+    private long solve(int i, int j, List<Integer> robot, int[][] factory) {
+        int n = robot.size();
+        int m = factory.length;
+
+        if (i == n) return 0;
+
+        if (j == m) return (long)1e15;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        long res = solve(i, j + 1, robot, factory);
+
+        long cost = 0;
+        int pos = factory[j][0];
+        int limit = factory[j][1];
+
+        for (int k = 0; k < limit && i + k < n; k++) {
+            cost += Math.abs(robot.get(i + k) - pos);
+            res = Math.min(res, cost + solve(i + k + 1, j + 1, robot, factory));
+        }
+
+        return dp[i][j] = res;
+    }
+}
